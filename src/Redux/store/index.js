5a34b33e99610dff9b62501aceb23de3fe85022e
Mohamad SayadArbabi <mrmsarbabi@gmail.com>
2020-04-13 11:02:14 +0430

    debug store

--- src/Redux/store/index.js
+++ src/Redux/store/index.js
@@ -1,12 +1,8 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { reducers } from "../reducer";
 import thunk from "redux-thunk";
 
 export const store = createStore(
     reducers,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(thunk)
 );
